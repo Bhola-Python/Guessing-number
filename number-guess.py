06f6f4f0627bb0c72ae3803567a77447a282c20c
Bhola Vishwakarma <vishwakabhola1@gmail.com>
2025-09-16 21:11:03 +0530

    Update number-guess.py

--- number-guess.py
+++ number-guess.py
@@ -4,14 +4,15 @@ import random # import random package
 def new_funct(): 
     random_ran = random.randint(1,100) # It randomly selects number from 1 to 100
     while True: # Keeps excuting untill specific condition is True
-        a = int(input('Enter your guess: '))
-        if a == random_ran:
+        input_int = int(input('Enter your guess: '))
+        if input_int == random_ran:
             print("You have matched Correct!")
             break
-        elif a < random_ran:
+        elif input_int < random_ran:
             print("Too Low")
         else:
             print("Too High")
 
 new_funct()
+
 
